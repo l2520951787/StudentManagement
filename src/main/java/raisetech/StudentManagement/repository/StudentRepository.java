84ea0540898720c480bfaa287c30d31c0051b71a
nishizaki <l2520951787as@gmail.com>
2026-02-15 22:56:46 +0900

    スタンダード第１６回演習課題

---
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -0,0 +1,53 @@
+package raisetech.StudentManagement.repository;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentsCourses;
+
+@Mapper
+public interface StudentRepository {
+
+  @Select("SELECT * FROM students")
+  List<Student> searchStudent();
+
+  @Select("SELECT * FROM students_courses")
+  List<StudentsCourses> searchStudentsCourses();
+
+  @Select("SELECT * FROM students WHERE id = #{id}")
+  Student searchStudentById(String id);
+
+  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
+  List<StudentsCourses> searchStudentsCoursesByStudentId(String studentId);
+
+  @Insert(
+      "INSERT INTO students(name, ruby, nickname, mail_address, area, age, gender, remark, is_deleted)"
+          + " VALUES(#{name}, #{ruby}, #{nickname}, #{mailAddress}, #{area}, #{age}, #{gender}, #{remark}, false)")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void registerStudent(Student student);
+
+  @Insert("INSERT INTO students_courses(student_id, course, start_date, end_date) "
+      + "VALUES(#{studentId}, #{course}, #{startDate}, #{endDate})")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void registerStudentsCourses(StudentsCourses studentsCourses);
+
+  @Update("UPDATE students SET "
+      + "name = #{name},"
+      + "ruby = #{ruby}, "
+      + "nickname = #{nickname},"
+      + "mail_address = #{mailAddress},"
+      + "area = #{area},"
+      + "age = #{age},"
+      + "gender = #{gender},"
+      + "remark = #{remark}"
+      + "WHERE id = #{id}")
+  void updateStudent(Student student);
+
+  @Delete("DELETE FROM students_courses WHERE student_id = #{studentId}")
+  void deleteStudentsCourses(String studentId);
+}
