cc9e25b8ec4609e57a843fbe6b020dbc7b330c1b
nishizaki <l2520951787as@gmail.com>
2026-02-15 22:56:46 +0900

    スタンダード第１６回演習課題

---
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -0,0 +1,84 @@
+package raisetech.StudentManagement.controller;
+
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import raisetech.StudentManagement.controller.converter.StudentConverter;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.domain.StudentDetail;
+import raisetech.StudentManagement.service.StudentService;
+
+@Controller
+public class StudentController {
+
+  private StudentService service;
+  private StudentConverter converter;
+
+  @Autowired
+  public StudentController(StudentService service, StudentConverter converter) {
+    this.service = service;
+    this.converter = converter;
+  }
+
+  @GetMapping("/studentList")
+  public String getStudentList(Model model) {
+    List<Student> students = service.searchStudentList();
+    List<StudentsCourses> studentsCourses = service.searchStudentsCourseList();
+
+    model.addAttribute("studentList", converter.convertStudentDetails(students, studentsCourses));
+    return "studentList";
+  }
+
+  @GetMapping("/studentsCourseList")
+  public List<StudentsCourses> getStudentsCourseList() {
+    return service.searchStudentsCourseList();
+  }
+
+  @GetMapping("/newStudent")
+  public String newStudent(Model model) {
+    StudentDetail studentDetail = new StudentDetail();
+    studentDetail.setStudentsCourses(Arrays.asList(new StudentsCourses()));
+    model.addAttribute("studentDetail", studentDetail);
+    return "registerStudent";
+  }
+
+  @PostMapping("/registerStudent")
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail,
+      BindingResult result) {
+    if (result.hasErrors()) {
+      return "registerStudent";
+    }
+
+    service.registerStudent(studentDetail);
+    System.out.println(studentDetail.getStudent().getName() + "さんが登録されました。");
+
+    return "redirect:/studentList";
+
+  }
+
+  @GetMapping("/studentList/{id}")
+  public String editStudent(@PathVariable String id, Model model) {
+    StudentDetail studentDetail = service.searchStudentDetail(id);
+    model.addAttribute("studentDetail", studentDetail);
+    return "updateStudent";
+  }
+
+  @PostMapping("/updateStudent")
+  public String updateStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+    if (result.hasErrors()) {
+      return "updateStudent";
+    }
+
+    service.updateStudent(studentDetail);
+    System.out.println(studentDetail.getStudent().getName() + "さんの情報が更新されました。");
+    return "redirect:/studentList";
+  }
+}
---
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -0,0 +1,65 @@
+package raisetech.StudentManagement.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.domain.StudentDetail;
+import raisetech.StudentManagement.repository.StudentRepository;
+
+@Service
+public class StudentService {
+
+  private StudentRepository repository;
+
+  @Autowired
+  public StudentService(StudentRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<Student> searchStudentList() {
+    return repository.searchStudent();
+  }
+
+  public List<StudentsCourses> searchStudentsCourseList() {
+    return repository.searchStudentsCourses();
+  }
+
+  public StudentDetail searchStudentDetail(String id) {
+    Student student = repository.searchStudentById(id);
+    List<StudentsCourses> studentsCourses = repository.searchStudentsCoursesByStudentId(id);
+
+    StudentDetail studentDetail = new StudentDetail();
+    studentDetail.setStudent(student);
+    studentDetail.setStudentsCourses(studentsCourses);
+
+    return studentDetail;
+  }
+
+  @Transactional
+  public void registerStudent(StudentDetail studentDetail) {
+    repository.registerStudent(studentDetail.getStudent());
+    for (StudentsCourses studentsCourses : studentDetail.getStudentsCourses()) {
+      studentsCourses.setStudentId(studentDetail.getStudent().getId());
+      studentsCourses.setStartDate(LocalDateTime.now());
+      studentsCourses.setEndDate(LocalDateTime.now().plusMonths(6));
+      repository.registerStudentsCourses(studentsCourses);
+    }
+  }
+
+  @Transactional
+  public void updateStudent(StudentDetail studentDetail) {
+    Student student = studentDetail.getStudent();
+    repository.updateStudent(student);
+    repository.deleteStudentsCourses(student.getId());
+    for (StudentsCourses studentsCourses : studentDetail.getStudentsCourses()) {
+      studentsCourses.setStudentId(studentDetail.getStudent().getId());
+      studentsCourses.setStartDate(LocalDateTime.now());
+      studentsCourses.setEndDate(LocalDateTime.now().plusMonths(6));
+      repository.registerStudentsCourses(studentsCourses);
+    }
+  }
+}
