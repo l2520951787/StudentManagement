40a0760f0d1f0a1d8084503721c1df96f9ccda23
nishizaki <l2520951787as@gmail.com>
2026-02-26 12:51:04 +0900

    スタンダード第１６回課題

---
+++ src/main/java/raisetech/StudentManagement/data/Student.java
@@ -0,0 +1,21 @@
+package raisetech.StudentManagement.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Student {
+
+  private String id;
+  private String name;
+  private String ruby;
+  private String nickname;
+  private String mailAddress;
+  private String area;
+  private int age;
+  private String gender;
+  private String remark;
+  private boolean isDeleted;
+
+}
